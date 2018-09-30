2e4f7c658be9d215096bfd38399a6aa344de0198
pschustek <philipp.schustek@gmail.com>
2018-09-30 22:07:50 +0200

    Improved modularization; added module agents.py

---
+++ agents.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 30 10:39:23 2018
+
+@author: User
+"""
+import numpy as np
+import scipy.stats as stats
+from scipy.optimize import minimize
+
+
+class Decision:
+
+    def __init__(self, nh, n):
+        self.nh = nh
+        self.nt = n - nh
+        self.n = n
+        self.num_trials = len(nh)
+        self.make_decision()
+        self.decision_correct = None
+
+    def make_decision(self):
+        q = np.divide(self.nh, self.n)
+        decision = np.zeros(self.num_trials)
+        decision[q > 0.5] = 1
+        # Break ties at random
+        ties = q == 0.5
+        decision[ties] = np.random.binomial(1, 0.5, np.sum(ties))
+        self.decision = decision
+
+    def decision_feedback(self, real_majority):
+        self.decision_correct = np.zeros(self.num_trials)
+        self.decision_correct[real_majority == self.decision] = 1
+
+    def align_to_decision(self, x):
+        return 2 * (self.decision - 0.5) * (x - 0.5) + 0.5
+
+
+
+class ProbabilisticConfidence(Decision):
+
+    def __init__(self, nh, n, h0=4, t0=4):
+        Decision.__init__(self, nh, n)
+        self.h0 = h0
+        self.t0 = t0
+
+        # Confidence in a majority of 'heads'
+        conf_h = 1 - stats.beta.cdf(0.5, self.nh + self.h0, self.nt + self.t0)
+        self.confidence_h = conf_h
+        self.confidence_d = self.align_to_decision(conf_h)
+
+
+
+class MappingBased(Decision):
+
+
+    def __init__(self, nh, n):
+        Decision.__init__(self, nh, n)
+        self.set_n = np.unique(n)
+        self.W = None
+
+
+    # Define parameterized stimulus-response mapping
+    def decision_confidence_map(dq, w):
+        return 1/(1+np.exp(-(w[0]*np.abs(dq-0.5)+w[1]*np.abs(dq-0.5)**3)))
+
+
+    def random_initial_weights(self, mu, sigma):
+        # Array for weights: (n,w)
+        self.W = np.random.normal(mu, sigma, (self.set_n.size, 2))
+
+
+    def confidence_batch_learning(self):
+        # Decision-aligned proportion of 'heads'
+        dq = self.align_to_decision(np.divide(self.nh, self.n))
+
+        # Initialize memory for tracking of the number of correct responses (C),
+        # the number of responses (N) and their quotient (Q) for each condition (dq, N)
+        Q, C, N = [], [], []
+        for j, n in enumerate(self.set_n):
+            # Possible decision-aligned proportion of 'heads' for each sample size
+            Q.append(np.arange(np.ceil(n/2), n+1) / n)
+            # Initialize to C/N = 0.5
+            C.append(np.ones(Q[j].size)*0.5)
+            N.append(np.ones(Q[j].size, dtype=int))
+
+        # Index (idn) for sample size n for all trials
+        M = self.n[:,np.newaxis] == self.set_n
+        n_indices = np.tile(np.arange(*self.set_n.shape), (self.num_trials, 1))[M]
+
+        # Cycle through trials
+        conf_batch = np.zeros(self.num_trials)
+        for t in range(self.num_trials):
+
+            # Select corresponding weights w by using boolean mask for sample size
+            idn = n_indices[t]
+
+            # Report decision confidence
+            conf_batch[t] = MappingBased.decision_confidence_map(dq[t], self.W[idn,:])
+
+            # Memorize outcomes
+            idq = Q[idn] == dq[t]
+            if self.decision_correct[t]:
+                C[idn][idq] += 1
+
+            N[idn][idq] += 1
+
+            # Train function
+            X = Q[idn]
+            Y = np.divide(C[idn], N[idn])
+            def ferr(w):
+                return np.sum((MappingBased.decision_confidence_map(X, w) - Y) ** 2)
+
+            res = minimize(ferr, self.W[idn,:], method='BFGS', jac=None,
+                           hess=None, options={'gtol': 1e-8, 'disp': False})
+            # Update
+            self.W[idn,:] = res.x
+
+        self.confidence_d = conf_batch
+
+
+    def confidence_sgd_learning(self, step_size):
+        # Simulate behavior Agent 2: On-line learning through stochastic gradient descent
+        # Same parameterized stimulus-response mapping as above
+        # Array for weights; (N,w)
+
+        # Decision-aligned proportion of 'heads'
+        dq = self.align_to_decision(np.divide(self.nh, self.n))
+
+        # Index (idn) for sample size n for all trials
+        M = self.n[:,np.newaxis] == self.set_n
+        n_indices = np.tile(np.arange(*self.set_n.shape), (self.num_trials, 1))[M]
+
+        # Cycle through trials
+        conf_sgd = np.zeros(self.num_trials)
+        for t in range(self.num_trials):
+
+            idn = n_indices[t]
+            w = self.W[idn,:]
+
+            # Report decision confidence (repeatedly used)
+            conf = MappingBased.decision_confidence_map(dq[t],w)
+            conf_sgd[t] = conf
+
+            # Gradient
+            derivative_base = 2*(conf-self.decision_correct[t])*conf*(1-conf)
+            grad = np.array([derivative_base*abs(dq[t]-0.5),
+                derivative_base*abs(dq[t]-0.5)**3])
+
+            # Adjust weights
+            self.W[idn,:] = self.W[idn,:] - step_size*grad
+
+        self.confidence_d = conf_sgd
--- demo_comparison_model_based_free_confidence_judgments.py
+++ demo_comparison_model_based_free_confidence_judgments.py
@@ -1,163 +1,101 @@
-# Demo of confidence judgments of two different learners
-
-"""Demo of confidence judgments of two different learners
+# -*- coding: utf-8 -*-
+"""
+Demo of confidence judgments of two different learners
 
 More text
 """
-# Import modules
 import numpy as np
-import scipy.stats as stats
 import matplotlib.pyplot as plt
-from scipy.optimize import minimize
-#import pandas as pd
+import agents as ag
+import sys
+import pdb
 
-# For reproducible results
-np.random.seed(262743)
 
-# %% Ideal observer makes posterior-based confidence reports
-def ideal_observer(nH, N, h=4, t=4):
-    """Posterior-based confidence reports"""
-    # Number of 'tails'
-    nT = N - nH
-    # Confidence in a majority of 'heads'
-    return 1 - stats.beta.cdf(0.5, nH + h, nT + t)
+def setup_task(beta_prior, num_trials):
 
-# %% Parameter definitions
-# To sample the 'coin bias'
-betaParams = 4
-numTrials = 2000
+    # Set of sample sizes
+    set_n = np.arange(6,13)
 
-# Set of sample sizes
-setN = np.arange(6,13)
-# Draws from multinomial distribution
-distN = np.random.multinomial(1, [1/setN.size]*setN.size, numTrials) == 1
+    # Draws from multinomial distribution
+    mask = np.random.multinomial(1, [1/set_n.size]*set_n.size, num_trials) == 1
 
-# Sample size for each trial
-num = np.tile(setN, (numTrials,1))[distN]
+    # Sample size for each trial
+    num = np.tile(set_n, (num_trials,1))[mask]
 
-# Sample 'airplane proportions'
-pH = np.random.beta(betaParams, betaParams, numTrials)
+    # Sample 'airplane proportions'
+    ph = np.random.beta(beta_prior, beta_prior, num_trials)
 
-# What is the airplane majority
-realMajority = pH > 0.5
-# Break ties at random; just in case
-ties = pH == 0.5
-realMajority[ties] = np.random.binomial(1, 0.5, np.sum(ties))
+    # What is the airplane majority
+    real_majority = np.zeros(num_trials)
+    real_majority[ph > 0.5] = 1
+    # Break ties at random; just in case
+    ties = ph == 0.5
+    real_majority[ties] = np.random.binomial(1, 0.5, np.sum(ties))
 
-# Sample the observations, number of 'heads'
-nH = np.random.binomial(num, pH)
-q = np.divide(nH,num)
+    # Sample the observations, number of 'heads'
+    nh = np.random.binomial(num, ph)
 
-# %% Simulate behavior
-# Confidence of ideal observer
-idealConfHeads = ideal_observer(nH, num)
+    return nh, num, real_majority, ph
 
-# Decisions (the same for all agents)
-decision = q > 0.5
-ties = q == 0.5
-decision[ties] = np.random.binomial(1, 0.5, np.sum(ties))
-# Needed for learning from feedback below
-decisionCorrect = realMajority == decision
 
-# Decision-aligned proportion of 'heads'
-dCq = 2*(decision-0.5)*(q-0.5) + 0.5
+def main(num_trials=600, seed=None, beta_prior=4, step=2):
+#    pdb.set_trace()
 
-# Probability of making correct choices
-dCIdeal = 2*(decision-0.5)*(idealConfHeads-0.5) + 0.5
+    # For reproducible results
+    np.random.seed(seed)
 
-# %% Agent 1 (Probabilistic)
-# but with mismatched prior distribution
-A1ConfHeads = ideal_observer(nH, num, 1, 1)
+    # Set up experiment
+    nh, num, real_majority, ph = setup_task(beta_prior, num_trials)
 
-# Confidence estimate of Agent 1
-dCA1 = 2*(decision-0.5)*(A1ConfHeads-0.5) + 0.5 
+    ideal = ag.ProbabilisticConfidence(nh, num, beta_prior, beta_prior)
+    dec = ideal.decision
 
-# %% Simulate behavior Agent 2: Batch learning
-# Define parameterized stimulus-response mapping
-def decision_confidence_A2(dq, w):
-    return 1/(1+np.exp(-(w[0]*np.abs(dq-0.5)+w[1]*np.abs(dq-0.5)**3)))  
+    # Confidence of ideal observer estimates the probability of making correct choices
+    prob_correct_choice = ideal.align_to_decision(ideal.confidence_h)
 
-# Array for weights; (N,w)
-A2_Wbatch = np.random.normal(0, 2, (setN.size,2))
-# Use same initialization for on-line learning
-A2_Wsgd = A2_Wbatch.copy()
+    # Decision rule is the same for all agents) except tie breaking
+    # We enforce the same tie breaking by copying
 
-# Initialize memory for tracking of the number of correct responses 
-# and the number of responses for each condition (dq, N)
-Q = []
-C = []
-N = []
-for j, n in enumerate(setN):
-    # Possible decision-aligned proportion of 'heads' for each sample size
-    Q.append(np.arange(np.ceil(n/2),n)/n)
-    # Initialize to C/N = 0.5
-    C.append(np.ones(Q[j].size)*0.5)
-    N.append(np.ones(Q[j].size, dtype=int))
+    # Probabilistic agent with mismatching Beta-prior distribution
+    prob_mismatched = ag.ProbabilisticConfidence(nh, num, 1, 1)
+    prob_mismatched.decision = dec
 
-# Cycle through trials
-A2ConfBatch = np.zeros(numTrials)
-for t in range(numTrials):
-    # Index ths trials' sample size in setN
-    idxN = [j for j,n in enumerate(setN) if n==num[t]][0]   # pick first element (only one)
-    # Select corresponding weights w by using boolean mask for sample size
-    maskN = num[0] == setN
-    w = A2_Wbatch[maskN,:].squeeze()
-    # Report decision confidence
-    A2ConfBatch[t] = decision_confidence_A2(dCq[t], w)
-    
-    # Memorize outcomes
-    idxq = Q[idxN] == dCq[t]
-    if decisionCorrect[t]:
-        C[idxN][idxq] = C[idxN][idxq] + 1
-    N[idxN][idxq] = N[idxN][idxq] + 1
-    
-    # Train function
-    X = Q[idxN]
-    Y = np.divide(C[idxN], N[idxN])
-    def ferr(w):
-        return np.sum((decision_confidence_A2(X, w) - Y) ** 2)
-    
-    res = minimize(ferr, w, method='BFGS',
-               jac=None, hess=None,
-               options={'gtol': 1e-8, 'disp': False})
-    A2_Wbatch[idxN,:] = res.x
-    
-# %% Simulate behavior Agent 2: On-line learning through stochastic gradient descent
-# Same parameterized stimulus-response mapping as above
-# Array for weights; (N,w)
-# A2_Wsgd is initialized above
-    
-# Cycle through trials
-A2ConfSGD = np.zeros(numTrials)
-for t in range(numTrials):
-    # Index ths trials' sample size in setN
-    idxN = [j for j, n in enumerate(setN) if n==num[t]][0]   # pick first element (only one)
-    # Select corresponding weights w by using boolean mask for sample size
-    maskN = num[0] == setN
-    w = A2_Wsgd[maskN,:].squeeze()
-    # Report decision confidence
-    A2ConfSGD[t] = decision_confidence_A2(dCq[t], w)
-    C = decision_confidence_A2  # just a short-cut
-    
-    # SGD
-    stepSize = 2
-    grad = np.array([2*(C(dCq[t],w)-decisionCorrect[t])*C(dCq[t],w)*(1-C(dCq[t],w))*abs(dCq[t]-0.5),
-        2*(C(dCq[t],w)-decisionCorrect[t])*C(dCq[t],w)*(1-C(dCq[t],w))*abs(dCq[t]-0.5)**3])
-    A2_Wsgd[idxN,:] = A2_Wsgd[idxN,:] - stepSize*grad
+    # Mapping based agent needs feedback to learn
+    mapping_batch = ag.MappingBased(nh, num)
+    mapping_batch.decision = dec
+    # Learning from feedback
+    mapping_batch.decision_feedback(real_majority)
+    mapping_batch.random_initial_weights(0, 2)
+    W0 = mapping_batch.W.copy()
+    mapping_batch.confidence_batch_learning()
 
-#%%
-def movmean(x, N):
-    return np.convolve(x, np.ones(N)/N)[(N-1):]
+    # Mapping based agent needs feedback to learn
+    mapping_sgd = ag.MappingBased(nh, num)
+    mapping_sgd.decision = dec
+    # Learning from feedback
+    mapping_sgd.decision_feedback(real_majority)
+    # Use same initial weights
+    mapping_sgd.random_initial_weights(0, 2) # TODO W0
+    mapping_sgd.confidence_sgd_learning(step)
 
-devA2Batch = np.abs(A2ConfBatch-dCIdeal)
-devA2SGD = np.abs(A2ConfSGD-dCIdeal)
-devA1 = np.abs(dCA1-dCIdeal)
+    def movmean(x, N):  # TODO handle edge effects
+        return np.convolve(x, np.ones(N)/N)[(N-1):]
 
-leg = plt.plot(np.arange(numTrials)+1, movmean(devA1,30),
-         np.arange(numTrials)+1, movmean(devA2Batch,30), 
-         np.arange(numTrials)+1, movmean(devA2SGD,30))
+    devA2Batch = np.abs(mapping_batch.confidence_d - prob_correct_choice)
+    devA2SGD = np.abs(mapping_sgd.confidence_d - prob_correct_choice)
+    devA1 = np.abs(prob_mismatched.confidence_d - prob_correct_choice)
 
-font = {'family': 'serif', 'size': 12}
-plt.xlabel('trial number', font)
-plt.ylabel('deviation', font)
-plt.legend(leg, ('A1', 'A2 batch', 'A2 sgd'), frameon=False)
+    num_trials = len(nh)    # TODO improve
+    leg = plt.plot(np.arange(num_trials)+1, movmean(devA1,30),
+             np.arange(num_trials)+1, movmean(devA2Batch,30),
+             np.arange(num_trials)+1, movmean(devA2SGD,30))
+
+    font = {'family': 'serif', 'size': 12}
+    plt.xlabel('trial number', font)
+    plt.ylabel('deviation', font)
+    plt.legend(leg, ('A1', 'A2 batch', 'A2 sgd'), frameon=False)
+
+
+if __name__ == '__main__':
+    pdb.set_trace()
+    main(*sys.argv[1:])
